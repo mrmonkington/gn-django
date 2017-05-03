6f2a846dfec83d551b0e52a402cddd46e3be19c4
Thomas Marchant <thomas.marchant@gamer-network.net>
2017-05-03 14:13:55 +0100

    unit test youtube methods

--- gn_django/video/youtube.py
+++ gn_django/video/youtube.py
@@ -12,7 +12,7 @@ def get_id(url):
         * A `string` if successful or `None` if not
     """
     if "youtu" in url:
-        match = re.search(r'^https?:\/\/youtu.be\/([A-Za-z0-9\-_]{11})$', url)
+        match = re.search(r'^https?:\/\/(?:www.)?youtu(?:.be|be.com\/[a-z]+)\/([A-Za-z0-9\-_]{11})(?:\?.+)?$', url)
         if match:
             return match.group(1)
         parsed = parse.urlparse(url)
