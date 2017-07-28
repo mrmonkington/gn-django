9c079ed98e2be0e33887575186b6079b1fdf9fac
Thomas Marchant <thomas.marchant@gamer-network.net>
2017-07-28 14:56:10 +0100

    basic autocomplete view

--- gn_django/form/autocomplete.py
+++ gn_django/form/autocomplete.py
@@ -1,4 +1,19 @@
-from dal import autocomplete
+from dal_select2.views import autocomplete as base_autocomplete
 
-class AutocompleteField(autocomplete.Select2QuerySetView):
-    pass
+class AutocompleteView(base_autocomplete.Select2QuerySetView):
+    def __init__(self, model, field, *args, **kwargs):
+        self.model = model
+        self.field = field
+        super(AutocompleteView, self).__init__(*args, **kwargs)
+
+    def get_queryset(self):
+        self.validate_usage()
+        models = self.model.all()
+        if self.q:
+            kwargs = {'%s__istartswith' % self.field: self.q}
+            models = models.filter(**kwargs)
+
+        return models
+
+    def validate_usage(self):
+        return
