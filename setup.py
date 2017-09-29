45ffa22dadafa73cb568925dd5df39f72be194d9
Brendan Smith <brendan.smith@gamer-network.net>
2017-09-29 09:37:57 +0100

    Bump to version 1.2.1

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup, find_packages
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 long_description = open(BASE_DIR + '/README.rst').read()
+print(find_packages(exclude=('tests', 'examples')))
 
 setup(
     name='gn-django',
-    version='1.2.0',
+    version='1.2.1',
     url='https://github.com/gamernetwork/gn-django',
     description='Collection of tools and patterns for Gamer Network django apps/projects.',
     long_description=long_description,
@@ -19,6 +20,8 @@ setup(
         'autocomplete': ['django-autocomplete-light==3.2.9'],
     },
     include_package_data=True,
+    author='Brendan Smith',
+    author_email='brendan.smith@gamer-network.net',
     scripts = ['gn_django/bin/gn_django', 'gn_django/bin/depstatus'],
     packages = find_packages(exclude=('tests', 'examples')),
 )
