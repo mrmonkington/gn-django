6a30fd22e9a926dea9ce05386b97749d634917d7
Brendan Smith <brendan.smith@gamer-network.net>
2017-10-17 12:14:17 +0100

    Test: Erroneously bump package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = open(BASE_DIR + '/README.rst').read()
 
 setup(
     name='gn-django',
-    version='1.4.0',
+    version='1.4.1',
     url='https://github.com/gamernetwork/gn-django',
     description='Collection of tools and patterns for Gamer Network django apps/projects.',
     long_description=long_description,
