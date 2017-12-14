a506f551fb26108a550d9228ad467340a0230fa1
Brendan Smith <brendan.smith@gamer-network.net>
2017-12-14 14:50:26 +0000

    Add new less compilations management command
    - Add lightweight `commands` django app for common helper django
      commands.
    - Adjust gulpfile to use `get_less_compilations` command - sourcing less
      locations from our django apps.

--- gn_django/bin/startproject/gulpfile.js
+++ gn_django/bin/startproject/gulpfile.js
@@ -6,6 +6,10 @@ var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var minify = require('gulp-minify-css');
 var util = require('gulp-util');
+var execSync = require('child_process').execSync;
+var merge = require('merge-stream');
+
+var compilations = JSON.parse(execSync('python manage.py get_less_compilations'));
 
 gulp.task('compile', function () {
   var l = less({});
@@ -13,20 +17,31 @@ gulp.task('compile', function () {
     console.log(e);
     l.end();
   });
-  return val = gulp.src([
-      './static/less/*.less',
-      '!./static/less/modules/**',
-      '!./static/less/helpers/**'
-    ])
+  var compilationTasks = [];
+  for (var i = 0; i < compilations.length; i++) {
+   var compilation = compilations[i];
+   var task = gulp.src(compilation['source'])
     .pipe(l)
     .pipe(minify())
     .pipe(autoprefixer({
       browsers: ['last 10 versions']
     }))
-    .pipe(gulp.dest('./static/css'))
-  ;
+    .pipe(gulp.dest(compilation['destination']));
+    compilationTasks.push(task);
+  }
+  var mergedTask = compilationTasks[0];
+  for (var i = 1; i < compilationTasks.length; i++) {
+    mergedTask = merge(mergedTask, compilationTasks[i]);
+  }
+  return mergedTask;
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./static/less/*.less', ['compile']);
+  var watchLocations = [];
+  for (var i = 0; i < compilations.length; i++) {
+    watchLocations.push(compilations[i]['watch']);
+  }
+  gulp.watch(watchLocations, ['compile']);
 });
+
+gulp.task('default', ['compile', 'watch']);
