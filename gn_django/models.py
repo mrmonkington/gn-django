0b59bbec6a5d6e3dd4e85ff9f8abd9b6ebbe486c
Thomas Marchant <thomas.marchant@gamer-network.net>
2018-07-25 15:07:40 +0100

    add searchable queryset mixin

---
+++ gn_django/models.py
@@ -0,0 +1,46 @@
+import re
+from django.db.models import Q
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+from .exceptions import ImproperlyConfigured
+
+class SearchableQuerySetMixin:
+    ignored_words = ['the', 'and']
+    min_length = 3
+
+    @property
+    def search_fields(self):
+        raise ImproperlyConfigured('`search_fields` attribute must be set')
+
+    def search(self, search_term, term_limit=5):
+        qs = self.all()
+        count = 0
+        terms = self._get_search_terms(search_term)
+        for term in terms:
+            term = term.strip('"')
+            if count >= term_limit:
+                raise ValidationError(_('Too many search terms, please use no more than %s. Note: Common words, repeated words, and words shorter than three characters are automatically removed and not counted.' % term_limit))
+            fields = self.search_fields
+            q = None
+            for field in self.search_fields:
+                field_q = Q(**{'%s__icontains' % field: term})
+                if q is None:
+                    q = field_q
+                else:
+                    q = q|field_q
+            matches = self.filter(q)
+            qs = qs & matches
+            count += 1
+        return qs.distinct()
+
+    def _get_search_terms(self, search_term):
+        # TODO genericise model searching in GN Django
+        phrase_pattern = re.compile(r'\"[^\"]+\"')
+        phrases = re.findall(phrase_pattern, search_term)
+        for phrase in phrases:
+            search_term = search_term.replace(phrase, '')
+        terms = search_term.split(' ')
+        terms = [t for t in terms if len(t) >= self.min_length and t not in self.ignored_words]
+        terms = set(phrases + terms)
+        return terms
