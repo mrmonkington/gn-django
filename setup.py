d9395c845da4897b5383417ef6ef81c697dcc695
John McDowell <john.mcdowell@gamer-network.net>
2019-05-31 11:05:35 +0100

    Update version numb er and django requirement

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ long_description = open(BASE_DIR + '/README.rst').read()
 
 setup(
     name='gn-django',
-    version='1.9.0',
+    version='2.0',
     url='https://github.com/gamernetwork/gn-django',
-    description='Collection of tools and patterns for Gamer Network django apps/projects.',
+    description=("Collection of tools and patterns for Gamer Network django "
+            "apps/projects."),
     long_description=long_description,
     install_requires=[
-        "django>=2.1,<2.2",
+        "django>=2.2,<2.3",
         "django-jinja==2.3.0",
     ],
     extras_require={
