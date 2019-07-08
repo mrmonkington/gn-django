09d25af0bb36411aac01ea2c7f90c4adead064a7
John McDowell <john.mcdowell@gamer-network.net>
2019-07-08 14:54:50 +0100

    Remove models from tests/core

--- tests/gn_django_tests/core/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-from gn_django.fields import UniquelyTrueBooleanField
-
-
-class TestUniquelyTrueBooleanModel(models.Model):
-    is_the_chosen_one = UniquelyTrueBooleanField(null=True, default=False)
